d2f4e2c6031eacce69916a540e5e8bbc4ee01614
Earl006 <earljoe06@gmail.com>
2024-11-01 14:45:46 +0300

    [Added]: produce module with restclient tests [Modified]: produce schema in schema.prisma

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 import categoryRoutes from './routes/category.routes';
+import produceRoutes from './routes/produce.routes';
 
 
 const cors = require('cors');
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/category', categoryRoutes);
+app.use('/api/product', produceRoutes);
 
 
 
