d4f4803590ea5150e8c8e519f34a93dd6c6306b5
Earl006 <earljoe06@gmail.com>
2024-11-07 14:31:44 +0300

    [Added]: backend

--- prisma/migrations/20241101204824_payment_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'COMPLETED', 'FAILED');
-
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "paymentId" TEXT,
-ADD COLUMN     "paymentMethod" TEXT NOT NULL DEFAULT 'MPESA',
-ADD COLUMN     "paymentStatus" "PaymentStatus" NOT NULL DEFAULT 'PENDING';
